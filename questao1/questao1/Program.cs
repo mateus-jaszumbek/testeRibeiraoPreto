5949251d9683f556aa4101f32f33e3d3f7c64c49
mateus jose jaszumbek <69019404+mateus-jaszumbek@users.noreply.github.com>
2024-03-06 20:26:10 -0300

    deploy do teste

---
+++ questao1/questao1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        Console.WriteLine("A soma dos números é: " + SOMA);
+    }
+}
